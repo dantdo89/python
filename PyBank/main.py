e46f75acc6b05f6b2e83cefdc4a3302c1db493cb
Dan T Do <dan@dantdo.com>
2022-02-27 21:50:40 -0500

    commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,52 +1,74 @@
-import os
-import csv 
+# Import Dependencies
+import os, csv
+from pathlib import Path 
 
-csvpath = os.path.join('/Users/dantdo/Desktop/GitHub/python-challenge/PyBank/Resources/budget_data.csv')
-totalmonth = 0
-totalprofitloss = 0
-previousprofitloss = 0
-profitlosschange = 0
-profitlosschangelist = []
-monthchange = []
-greatestincrease = 0
-greatestdecrease = 1000
+# Declare file location through pathlib
+input_file = Path("python-challenge", "PyBank", "budget_data.csv")
 
-with open(csvpath) as profitlossdata:
-   reader = csv.DictReader(profitlossdata)
-   index=0
-   for row in reader:
-       if(index==0):
-           totalmonth+=1
-           totalprofitloss = totalprofitloss + int(row["Profit/Losses"])
-           previousprofitloss = int(row["Profit/Losses"])
-           monthchange = monthchange + [row["Date"]]
-           index+=1
-           continue
+# Create empty lists to iterate through specific rows for the following variables
+total_months = []
+total_profit = []
+monthly_profit_change = []
+ 
+# Open csv in default read mode with context manager
+with open(input_file,newline="", encoding="utf-8") as budget:
 
-       totalmonth = totalmonth + 1
-       totalprofitloss = totalprofitloss + int(row["Profit/Losses"])
-       profitlosschange = int(row["Profit/Losses"]) - previousprofitloss
-       profitlosschangelist.append(profitlosschange)
-       previousprofitloss = int(row["Profit/Losses"])
-       monthchange = monthchange + [row["Date"]]
-      
-   greatestdecrease=min(profitlosschangelist)
-   greatestincrease=max(profitlosschangelist)
-   
-   greatestdecreasemonth=profitlosschangelist.index(greatestdecrease)+1
-   greatestincreasemonth=profitlosschangelist.index(greatestincrease)+1
+     # Store the contents of budget_data.csv in the variable csvreader
+    csvreader = csv.reader(budget,delimiter=",") 
 
-   print("*")
-   
-   print("Financial Analysis")
+    # Skip the header labels to iterate with the values
+    header = next(csvreader)  
 
-   print("*")
+    # Iterate through the rows in the stored file contents
+    for row in csvreader: 
 
-   print(f"Total Months: {totalmonth}\n")
-   
-   print(f"Total Profit/Losess: ${totalprofitloss}\n")
-   print(f"Average Change: ${round(sum(profitlosschangelist)/len(profitlosschangelist),2)}")
+        # Append the total months and total profit to their corresponding lists
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
 
-   print(f"Greatest increase in Profits: {monthchange[ greatestincreasemonth]} (${(str(greatestincrease))})")
+    # Iterate through the profits in order to get the monthly change in profits
+    for i in range(len(total_profit)-1):
+        
+        # Take the difference between two months and append to monthly profit change
+        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
+        
+# Obtain the max and min of the the montly profit change list
+max_increase_value = max(monthly_profit_change)
+max_decrease_value = min(monthly_profit_change)
 
-   print(f"Greatest decrease in Profits: {monthchange[greatestdecreasemonth]} (${(str(greatestdecrease))})")
+# Correlate max and min to the proper month using month list and index from max and min
+#We use the plus 1 at the end since month associated with change is the + 1 month or next month
+max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
+
+# Print Statements
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {len(total_months)}")
+print(f"Total: ${sum(total_profit)}")
+print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+
+# Output files
+output_file = Path("python-challenge", "PyBank", "Financial_Analysis_Summary.txt")
+
+with open(output_file,"w") as file:
+    
+# Write methods to print to Financial_Analysis_Summary 
+    file.write("Financial Analysis")
+    file.write("\n")
+    file.write("----------------------------")
+    file.write("\n")
+    file.write(f"Total Months: {len(total_months)}")
+    file.write("\n")
+    file.write(f"Total: ${sum(total_profit)}")
+    file.write("\n")
+    file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+    file.write("\n")
+    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+    file.write("\n")
+    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+
+
